319e323636640943588459e007c7deb2b56baff9
Ethan Niles <notethansemail123456789@gmail.com>
2026-02-06 19:59:07 -0500

    changed motor.h to be consistent with use in motor.c

--- flight-team/uav/main/Motor/motor.h
+++ flight-team/uav/main/Motor/motor.h
@@ -1,17 +1,17 @@
 #include "esp_err.h"
+#include <cstdint>
 #include <stdint.h>
 
 typedef struct {
-  int pin;           // GPIO pin motor will run on
-  float motor_speed_hz; // Must be positive please
-  char* task_name;
+  int pin; // GPIO pin motor will run on
+  char *task_name;
 } motor_config;
 
 // motor interface used by user
 typedef void *motor_handler;
 
 // creates motor and starts motor thread
-motor_handler* init_motor(motor_config *);
+motor_handler *init_motor(motor_config *);
 
 // Sets motor speed as long as speed is valid
 esp_err_t set_motor_speed(motor_handler *, uint32_t);
